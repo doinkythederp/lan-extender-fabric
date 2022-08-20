2c75454f73e0a463057704fb3b7a7fe469e976df
doinkythederp <doinkythederp@icloud.com>
2022-08-20 09:37:11 -0700

    chore: remove unused import

--- src/main/java/me/doinkythederp/lanextender/mixin/ScreenAccessor.java
+++ src/main/java/me/doinkythederp/lanextender/mixin/ScreenAccessor.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
-import org.spongepowered.asm.mixin.gen.Invoker;
 
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.Drawable;
